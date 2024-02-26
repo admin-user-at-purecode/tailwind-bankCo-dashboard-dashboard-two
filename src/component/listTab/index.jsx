2071e6193c84d29ed597a5529d9ba5b348ca6257
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-26 17:13:58 +0530

    component and screenshots added

--- src/component/listTab/index.jsx
+++ src/component/listTab/index.jsx
@@ -6,8 +6,8 @@ import UserTab from "./UserTab";
 
 function ListTab({ pageSize }) {
   return (
-    <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
-      <div className="flex flex-col space-y-5">
+    <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600 ">
+      <div className="flex flex-col space-y-5 ">
         <div className="flex h-[56px] w-full space-x-4">
           <Search />
           <Filter options={["January", "February", "March"]} />
