794071f2537a702cf41fb65ae0cd76ad951e76de
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-28 17:50:40 +0530

    screenshots added

--- src/pages/homeTwo/index.jsx
+++ src/pages/homeTwo/index.jsx
@@ -8,10 +8,10 @@ import Location from "../../component/summary/Location";
 
 function HomeTwo() {
     return (
-        <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
+        <main className="w-full px-20 pb-6 pt-[100px] sm:pt-[156px] xl:px-30 xl:pb-12 mx-20 ">
             {/* write your code here */}
-            <div className="2xl:flex 2xl:space-x-[48px]">
-                <section className="2xl:flex-1 2xl:mb-0 mb-6">
+            <div className="2xl:flex 2xl:space-x-[48px] ">
+                <section className="2xl:flex-1 2xl:mb-0 mb-6 ">
                     <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
                         <TotalWidgetV2 />
                         <Efficiency height="h-[180px]" />
