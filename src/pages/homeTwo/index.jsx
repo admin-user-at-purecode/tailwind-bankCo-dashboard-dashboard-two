b489d5f4ce2ddf17e654b54995008bf13d926773
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-28 14:54:31 +0530

    component and screenshot added

--- src/pages/homeTwo/index.jsx
+++ src/pages/homeTwo/index.jsx
@@ -7,29 +7,31 @@ import Summary from "../../component/summary";
 import Location from "../../component/summary/Location";
 
 function HomeTwo() {
-    return (
-        <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-            {/* write your code here */}
-            <div className="2xl:flex 2xl:space-x-[48px]">
-                <section className="2xl:flex-1 2xl:mb-0 mb-6">
-                    <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
-                        <TotalWidgetV2 />
-                        <Efficiency height="h-[180px]" />
-                    </div>
-                    <div className="w-full mb-[24px] flex space-x-[24px]">
-                        <Summary />
-                        <Location />
-                    </div>
+  return (
+    <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
+      {/* write your code here */}
+      <div className="2xl:flex 2xl:space-x-[48px]">
+        <section className="2xl:flex-1 2xl:mb-0 mb-6">
+          <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
+            <TotalWidgetV2 />
+            <Efficiency height="h-[180px]" />
+          </div>
+          <div className="w-full mb-[24px] flex space-x-[24px]">
+            <Summary />
+            <Location />
+          </div>
 
-                    <ListTab />
-                </section>
-                <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
-                    <Wallet />
-                    <Calender />
-                </section>
-            </div>
-        </main>
-    );
+          <ListTab />
+        </section>
+        <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
+          <Wallet />
+          <Calender />
+        </section>
+      </div>
+      <br/>
+      <Summary />
+    </main>
+  );
 }
 
 export default HomeTwo;
